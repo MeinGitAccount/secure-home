58413d1e6e6139958951728ed990094e3aace64f
alinemavie <a.sageder111@gmail.com>
2023-06-22 23:10:15 +0200

    setup of frontend

--- src/main/java/com/jku/at/securehome/SecureHomeApplication.java
+++ src/main/java/com/jku/at/securehome/SecureHomeApplication.java
@@ -2,9 +2,13 @@ package com.jku.at.securehome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//exclude = { SecurityAutoConfiguration.class, ReactiveSecurityAutoConfiguration.class }
-@SpringBootApplication
+
+@SpringBootApplication(
+	exclude = { SecurityAutoConfiguration.class, ReactiveSecurityAutoConfiguration.class }
+)
 public class SecureHomeApplication {
 
 	public static void main(String[] args) {
